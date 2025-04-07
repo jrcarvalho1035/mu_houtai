60c10be5e73ebc5208a28caf565d8452023e1a51
jrcarvalho1035 <55264262+jrcarvalho1035@users.noreply.github.com>
2025-04-06 21:01:44 -0300

    Create activitycommon.lua

---
+++ data/config/activity/activitycommon.lua
@@ -0,0 +1,14 @@
+-- from activity.xlsx 活动辅助表 
+ActivityCommonConfig={
+	daily60head = Lang.ActivityCommonConfig_daily60head_45,
+	daily60content = Lang.ActivityCommonConfig_daily60content_46,
+	dialconversion = 0.88,
+	dialtimesneed = 1000,
+	actRankPowers = {50000,50000,50000,50000,50000,50000,50000},
+	act35Recharge = 3000,
+	act35LuckyScore = 100,
+	type20rank3id = {[10700]=10704,[10705]=10709,},
+	type20NextNoticeId = {[10700]=53,[10705]=53,},
+	type20rank3NoticeId = {[1]=50,[2]=51,[3]=52,},
+	type20pv = {[10700]=10701,[10705]=10706,},
+}
