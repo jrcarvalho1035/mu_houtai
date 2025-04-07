1617292135104a1e7c2262a5d6d03854c2b6d57d
jrcarvalho1035 <55264262+jrcarvalho1035@users.noreply.github.com>
2025-04-06 21:02:41 -0300

    Create type20sub.lua

---
+++ data/config/activity/type20sub.lua
@@ -0,0 +1,33 @@
+-- from activity.xlsx 20副表 
+ActivityType20SubConfig={
+	[9100]={
+		id = 9100,
+		zhuti = Lang3.ActivityType20Config_1,
+		zhuti2 = Lang3.ActivityType20Config_2,
+	},
+	[9105]={
+		id = 9105,
+		zhuti = Lang3.ActivityType20Config_1,
+		zhuti2 = Lang3.ActivityType20Config_2,
+	},
+	[9900]={
+		id = 9900,
+		zhuti = Lang3.ActivityType20Config_1,
+		zhuti2 = Lang3.ActivityType20Config_2,
+	},
+	[9905]={
+		id = 9905,
+		zhuti = Lang3.ActivityType20Config_1,
+		zhuti2 = Lang3.ActivityType20Config_2,
+	},
+	[10700]={
+		id = 10700,
+		zhuti = Lang3.ActivityType20Config_1,
+		zhuti2 = Lang3.ActivityType20Config_2,
+	},
+	[10705]={
+		id = 10705,
+		zhuti = Lang3.ActivityType20Config_1,
+		zhuti2 = Lang3.ActivityType20Config_2,
+	},
+}
