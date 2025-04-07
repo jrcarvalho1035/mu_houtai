607f1f183f315d6d0c726dc9266433a4c980a1ee
jrcarvalho1035 <55264262+jrcarvalho1035@users.noreply.github.com>
2025-04-06 21:02:35 -0300

    Create type12.lua

---
+++ data/config/activity/type12.lua
@@ -0,0 +1,273 @@
+-- from activity.xlsx 12活动掉落 
+ActivityType12Config={
+	[1220]={
+	[1]={
+		id = 1220,
+		index = 1,
+		subType = 3,
+		sType = 1,
+		dropindex = 1,
+	},
+	},
+	[1160]={
+	[1]={
+		id = 1160,
+		index = 1,
+		subType = 4,
+		sType = 3,
+		dropindex = 1,
+	},
+	},
+	[1161]={
+	[1]={
+		id = 1161,
+		index = 1,
+		subType = 4,
+		sType = 6,
+		dropindex = 1,
+	},
+	},
+	[1162]={
+	[1]={
+		id = 1162,
+		index = 1,
+		subType = 4,
+		sType = 7,
+		dropindex = 1,
+	},
+	},
+	[9135]={
+	[1]={
+		id = 9135,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 3,
+	},
+	},
+	[9295]={
+	[1]={
+		id = 9295,
+		index = 1,
+		subType = 4,
+		sType = 3,
+		dropindex = 1,
+	},
+	},
+	[9296]={
+	[1]={
+		id = 9296,
+		index = 1,
+		subType = 4,
+		sType = 6,
+		dropindex = 1,
+	},
+	},
+	[9297]={
+	[1]={
+		id = 9297,
+		index = 1,
+		subType = 4,
+		sType = 7,
+		dropindex = 1,
+	},
+	},
+	[9298]={
+	[1]={
+		id = 9298,
+		index = 1,
+		subType = 4,
+		sType = 4,
+		dropindex = 1,
+	},
+	},
+	[9299]={
+	[1]={
+		id = 9299,
+		index = 1,
+		subType = 4,
+		sType = 5,
+		dropindex = 1,
+	},
+	},
+	[9310]={
+	[1]={
+		id = 9310,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 4,
+	},
+	},
+	[9550]={
+	[1]={
+		id = 9550,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 2,
+	},
+	},
+	[9760]={
+	[1]={
+		id = 9760,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 6,
+	},
+	},
+	[9935]={
+	[1]={
+		id = 9935,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 3,
+	},
+	},
+	[10095]={
+	[1]={
+		id = 10095,
+		index = 1,
+		subType = 4,
+		sType = 3,
+		dropindex = 1,
+	},
+	},
+	[10096]={
+	[1]={
+		id = 10096,
+		index = 1,
+		subType = 4,
+		sType = 6,
+		dropindex = 1,
+	},
+	},
+	[10097]={
+	[1]={
+		id = 10097,
+		index = 1,
+		subType = 4,
+		sType = 7,
+		dropindex = 1,
+	},
+	},
+	[10098]={
+	[1]={
+		id = 10098,
+		index = 1,
+		subType = 4,
+		sType = 4,
+		dropindex = 1,
+	},
+	},
+	[10099]={
+	[1]={
+		id = 10099,
+		index = 1,
+		subType = 4,
+		sType = 5,
+		dropindex = 1,
+	},
+	},
+	[10110]={
+	[1]={
+		id = 10110,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 4,
+	},
+	},
+	[10350]={
+	[1]={
+		id = 10350,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 2,
+	},
+	},
+	[10560]={
+	[1]={
+		id = 10560,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 6,
+	},
+	},
+	[10735]={
+	[1]={
+		id = 10735,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 3,
+	},
+	},
+	[10895]={
+	[1]={
+		id = 10895,
+		index = 1,
+		subType = 4,
+		sType = 3,
+		dropindex = 1,
+	},
+	},
+	[10896]={
+	[1]={
+		id = 10896,
+		index = 1,
+		subType = 4,
+		sType = 6,
+		dropindex = 1,
+	},
+	},
+	[10897]={
+	[1]={
+		id = 10897,
+		index = 1,
+		subType = 4,
+		sType = 7,
+		dropindex = 1,
+	},
+	},
+	[10898]={
+	[1]={
+		id = 10898,
+		index = 1,
+		subType = 4,
+		sType = 4,
+		dropindex = 1,
+	},
+	},
+	[10899]={
+	[1]={
+		id = 10899,
+		index = 1,
+		subType = 4,
+		sType = 5,
+		dropindex = 1,
+	},
+	},
+	[10910]={
+	[1]={
+		id = 10910,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 4,
+	},
+	},
+	[11150]={
+	[1]={
+		id = 11150,
+		index = 1,
+		subType = 2,
+		sType = 1,
+		dropindex = 2,
+	},
+	},
+}
